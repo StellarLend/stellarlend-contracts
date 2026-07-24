e0b7d50ec6e55fdfb28d03e87602868f950315ed
Developer <developer@teachlink.com>
2026-07-24 11:00:05 -0700

    Wire vesting test modules into lib.rs

--- stellar-lend/contracts/vesting/src/lib.rs
+++ stellar-lend/contracts/vesting/src/lib.rs
@@ -371,4 +371,32 @@ impl VestingContract {
 }
 
 #[cfg(test)]
+mod accelerate_test;
+#[cfg(test)]
+mod claimable_consistency_test;
+#[cfg(test)]
+mod cliff_bound_test;
+#[cfg(test)]
+mod grant_transfer_test;
+#[cfg(test)]
+mod lifecycle_e2e_test;
+#[cfg(test)]
+mod milestone_schedule_test;
+#[cfg(test)]
+mod multi_grant_test;
+#[cfg(test)]
+mod partial_claim_test;
+#[cfg(test)]
 mod pause_offset_test;
+#[cfg(test)]
+mod pause_test;
+#[cfg(test)]
+mod revoke_split_test;
+#[cfg(test)]
+mod vested_at_proptest;
+#[cfg(test)]
+mod vesting_contract_test;
+#[cfg(test)]
+mod vesting_doc_example_test;
+#[cfg(test)]
+mod vesting_views_test;
